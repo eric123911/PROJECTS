0d286ad566c7e76eff712715e82df2d86380d9e2
eric123911 <eric_schneller@hotmail.com>
2018-09-20 18:27:10 +0200

    added new functions

--- libft/includes/libft.h
+++ libft/includes/libft.h
@@ -5,11 +5,28 @@
 # include <unistd.h>
 
 
-// PRINT FUNCTION PROTOTYPES
+// LibMEM PROTOTYPES
+void	ft_swap(int *a, int *b);
+
+// LibNUMBERS PROTOTYPES
+int		ft_atoi(char *str);
+void	ft_putnbr(int n);
+
+// LibPRINT PROTOTYPES
 void	ft_putchar(char c);
 void	ft_putstr(char *str);
 
-// STRINGS FUNCTION PROTOYPES
+// LibSTRINGS PROTOYPES
+int		ft_strcmp(const char *s1, const char *s2);
 int		ft_strlen(char *str);
+int		ft_toupper(int c);
+int		ft_toupper(int c);
+
+// LibUTIL PROTOTYPES
+int		ft_isalnum(int c);
+int		ft_isalpha(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
 
 #endif
