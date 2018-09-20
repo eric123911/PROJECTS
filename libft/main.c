4bcc3de8ad4eee8beb587308d10a63df1f7a9d73
eric123911 <eric_schneller@hotmail.com>
2018-09-20 18:27:47 +0200

    added basic tests

--- libft/main.c
+++ libft/main.c
@@ -1,7 +1,25 @@
 #include "includes/libft.h"
 
-int	main(void)
+#define _AV_ATOI ft_atoi(av[1])
+
+int	main(int ac, char **av)
 {
-  ft_putstr("Hello world !");
-  return (0);
+	if (ac < 2)
+	{
+		ft_putstr("Please specify at least an argument.\n");
+		return (-1);
+	}
+	ft_putnbr(ft_atoi("    -42   #@'è_ç"));
+	ft_putchar('\n');
+	ft_putstr("Hello world !");
+	ft_putstr("\n==========================");
+	ft_putstr("\nav[1][0] isalnum: "), ft_putnbr(ft_isalnum(_AV_ATOI));
+	ft_putstr("\nav[1][0] isalpha: "), ft_putnbr(ft_isalpha(_AV_ATOI));
+	ft_putstr("\nav[1][0] isdigit: "), ft_putnbr(ft_isdigit(_AV_ATOI));
+	ft_putstr("\nav[1][0] isprint: "), ft_putnbr(ft_isprint(_AV_ATOI));
+	ft_putstr("\nav[1][0] isascii: "), ft_putnbr(ft_isascii(_AV_ATOI));
+
+	// Just to suppress [-Werror=unused-parameter]
+	av[0] = "";
+	return (0);
 }
