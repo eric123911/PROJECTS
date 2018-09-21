ddcdafa1d1dc0c0b904ca48d7479d0148eb1e263
eric123911 <eric_schneller@hotmail.com>
2018-09-21 10:59:06 +0200

    added putendl, bzero & strdup :)

---
+++ libft/LibStrings/ft_strdup.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   ft_strdup.c                                      .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: eschnell <eschnell@le-101.fr>              +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2018/09/21 10:12:09 by eschnell     #+#   ##    ##    #+#       */
+/*   Updated: 2018/09/21 10:48:44 by eschnell    ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#include "../includes/libft.h"
+
+char		*ft_strdup(const char *s)
+{
+	int		i;
+	char	*cpy;
+
+	i = 0;
+	cpy = (char *)malloc(sizeof(*s) * (ft_strlen((char *)s) + 1));
+	while (s[i])
+	{
+		cpy[i] = s[i];
+		i++;
+	}
+	cpy[i] = '\0';
+	return (cpy);
+}
--- libft/includes/libft.h
+++ libft/includes/libft.h
@@ -2,31 +2,37 @@
 # define LIBFT_H
 
 // SYSTEM LIBRARIES
+# include <string.h>
 # include <unistd.h>
+# include <stdlib.h>
 
+typedef struct		s_list
+{
+	void			*content;
+	size_t			content_size;
+	struct s_list	*next;
+}					t_list;
 
 // LibMEM PROTOTYPES
 void	ft_swap(int *a, int *b);
-
 // LibNUMBERS PROTOTYPES
 int		ft_atoi(char *str);
 void	ft_putnbr(int n);
-
 // LibPRINT PROTOTYPES
 void	ft_putchar(char c);
+void	ft_putendl(char const *s);
 void	ft_putstr(char *str);
-
 // LibSTRINGS PROTOYPES
+void	ft_bzero(void *s, size_t n);
 int		ft_strcmp(const char *s1, const char *s2);
+char	*ft_strdup(const char *s);
 int		ft_strlen(char *str);
 int		ft_toupper(int c);
 int		ft_toupper(int c);
-
 // LibUTIL PROTOTYPES
 int		ft_isalnum(int c);
 int		ft_isalpha(int c);
 int		ft_isascii(int c);
 int		ft_isdigit(int c);
 int		ft_isprint(int c);
-
 #endif
